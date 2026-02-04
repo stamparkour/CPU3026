a80259e1e39ea385fe8f162a7888b00dc6e6c049
stamparkour <42157566+stamparkour@users.noreply.github.com>
2026-02-03 21:24:10 -0500

    Project Builds
    added all warnings and warnings as errors
    added source files.
    added cpu3026_config.h.in file for cmake project

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,6 +1,13 @@
 #assumes include(FetchContent)
 
 message("importing gtest...")
-FetchContent_Declare()
-FetchContent_MakeAvailable()
+FetchContent_Declare(
+  googletest
+  #GIT_REPOSITORY https://github.com/google/googletest.git
+  #GIT_TAG 7c3b4d54e3294990d9c3de583fccff97116efb6b
+  URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
+  DOWNLOAD_EXTRACT_TIMESTAMP
+)
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
 message("gtest found!")
