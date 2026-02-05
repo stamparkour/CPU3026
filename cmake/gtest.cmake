82f7285885876904a50698982052a3a6d53e6062
stamparkour <42157566+stamparkour@users.noreply.github.com>
2026-02-04 21:31:48 -0500

    fixed google test failing on windows

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,6 +1,7 @@
 #assumes include(FetchContent)
 
 message("importing gtest...")
+set(BUILD_SHARED_LIBS OFF)
 FetchContent_Declare(
   googletest
   URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
@@ -8,4 +9,5 @@ FetchContent_Declare(
 )
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
+set(BUILD_SHARED_LIBS ON)
 message("gtest found!")
